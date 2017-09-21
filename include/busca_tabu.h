a423c022ea0f7aefd204dc160962acf4d9203fe9
Válber Laux <valber@ufrrj.br>
2017-09-21 10:33:27 -0300

    Aumenta e diminui tamanho da lista tabu dinamicamente

--- include/busca_tabu.h
+++ include/busca_tabu.h
@@ -5,13 +5,21 @@
 #include "cliente.h"
 #include "instancia.h"
 #include "utils.h"
+#include "lista_tabu.h"
 
 using namespace std;
 
 
 class BuscaTabu
 {
+    private:
+        ListaTabu* lista_tabu;
+        int tam_maximo_lista = 100; // receber via param; tem que ser dinâmico!
+        int ultimo_custo_avaliado = INT32_MAX;
+        int iter_until_update = 20; //receber via param de conf
+        void avalia_tamanho_lista_tabu(Solucao s_best, int iter);
     public:
+        BuscaTabu();
         Solucao executa(Instancia &ins);
 };
 
--- src/busca_tabu.cpp
+++ src/busca_tabu.cpp
@@ -7,19 +7,52 @@
 
 using namespace std;
 
+BuscaTabu::BuscaTabu()
+{
+    lista_tabu = new ListaTabu(tam_maximo_lista);
+}
+
 Solucao BuscaTabu::executa(Instancia &ins)
 {
+    Solucao s_best(ins);
     Solucao s(ins);
     s = Grasp(false).constroi_solucao(ins, 1);
 
-    int tam_max_lista = ins.get_n_clientes() * 2;
-    ListaTabu* lista_tabu = new ListaTabu(tam_max_lista);
-
     Vnd vnd(lista_tabu);
     int k_max = 5;
-    vnd.executa(s, ins, k_max);
+
+    int iter = 0, iter_sem_melhora = ins.get_n_clientes();
+    while(iter < iter_sem_melhora)
+    {
+        s = vnd.executa(s, ins, k_max);
+        if (s.get_custo() < s_best.get_custo()) 
+        {
+            s_best = s;
+            iter_sem_melhora = 0;
+        }
+        else
+        {
+            s = s_best;
+        }
+
+        avalia_tamanho_lista_tabu(s_best, iter);
+
+        iter++;
+    }
 
     delete lista_tabu;
 
     return s;
+}
+
+void BuscaTabu::avalia_tamanho_lista_tabu(Solucao s_best, int iter)
+{
+    if (iter % iter_until_update == 0) 
+    {
+        if (s_best.get_custo() < ultimo_custo_avaliado)
+            lista_tabu->diminui_lista();
+
+        else
+            lista_tabu->aumenta_lista();
+    }
 }
