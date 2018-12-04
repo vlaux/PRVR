711c542fca9732ebbf2c2335a356bf8f7f289459
Válber Laux <valber@ufrrj.br>
2018-12-04 21:39:50 -0200

    main atualizado para todos os tipos de execução

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,161 @@
-// #include <iostream>  
-// #include <vector>
-// #include <algorithm>
-// #include <cstdlib>
-// #include <ctime>
-// #include "leitor_arquivos.h"
-// #include "instancia.h"
-// #include "solucao.h"
-// #include "grasp.h"
-// #include "ils.h"
-// #include "vns.h"
-// #include "busca_tabu.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include "leitor_arquivos.h"
+#include "instancia.h"
+#include "solucao.h"
+#include "grasp.h"
+#include "ils.h"
+#include "vns.h"
+#include "vnd.h"
+#include "busca_tabu.h"
+#include "construtor.h"
+#include "validador.h"
+#include "busca_local.h"
+#include "utils.h"
+#include "logger.h"
 
-// using namespace std;
+using namespace std;
 
-// int main(int argc, char *argv[])
-// {
-//     srand(time(NULL));
+Logger *logger = Logger::get_instance();
 
-//     char *nome_arquivo = argv[1];
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
 
-//     Instancia ins = le_arquivo(nome_arquivo);
+    char *nome_arquivo = argv[1];
 
-//     char *modo = argv[2];
-    
-//     if (strcmp(modo, "GRASP") == 0) {
-//         cout << "Executando GRASP para instância " << nome_arquivo << endl;
-//         int n_iter = *argv[3];
-//         Solucao s = Grasp(true, n_iter).executa(ins, argv);
-//         s.imprime();
-//     }
-//     else if (strcmp(modo, "ILS") == 0) {
-//         cout << "Executando ILS para instância " << nome_arquivo << endl;
-        
-//         Solucao sol_inicial(ins);
-//         sol_inicial = Grasp(false, 0, 0, 1).constroi_solucao(ins);
+    Instancia *ins = le_arquivo(nome_arquivo);
 
-//         int max_iter = atoi(argv[3]);
-//         Solucao s = Ils().executa(ins, sol_inicial, max_iter, argv);
-//         s.imprime();
-//     }
-//     else if (strcmp(modo, "VNS") == 0) {
-//         cout << "Executando VNS para instância " << nome_arquivo << endl;
-        
-//         Solucao sol_inicial(ins);
-//         sol_inicial = Grasp(false, 0, 0, 1).constroi_solucao(ins);
+    Solucao solucao_final(ins);
 
-//         int k_max = atoi(argv[3]);
-//         char* tipo_bl = argv[4];
-//         int k_max_bl = atoi(argv[5]);
-//         Solucao s = Vns(tipo_bl, k_max_bl).executa(sol_inicial, ins, k_max);
-//         s.imprime();
-//     }
-//     else if (strcmp(modo, "TABU") == 0) {
-//         cout << "Executando TABU para instância " << nome_arquivo << endl;
-        
-//         Solucao sol_inicial(ins);
-//         sol_inicial = Grasp(false, 0, 0, 1).constroi_solucao(ins);
+    char *metaheuristica = argv[2];
 
-//         int max_iter = atoi(argv[3]);
-//         char* tipo_bl = argv[4];
-//         int k_max_bl = atoi(argv[5]);
-//         Solucao s = BuscaTabu(tipo_bl, k_max_bl).executa(ins, sol_inicial, max_iter);
-//         s.imprime();
-//     }
-//     else {
-//         cout << "Heurística desconhecida" << endl;
-//         abort();
-//     }
+    // --------------- GRASP ---------------
+    if (strcmp(metaheuristica, "GRASP") == 0)
+    {
+        float alpha = atof(argv[3]);
+        int iter_grasp = atoi(argv[4]);
 
-//     return EXIT_SUCCESS;
-// }
+        BuscaLocal *bl = nullptr;
+        BuscaLocal *bl_aux = nullptr;
+
+        char *metodo_bl = argv[5];
+
+        // ----------- com VNS -----------
+        if (strcmp(metodo_bl, "VNS") == 0)
+        {
+            int iter_vns = atoi(argv[6]);
+            bl = new Vns(iter_vns);
+        }
+
+        // ----------- com VND -----------
+        if (strcmp(metodo_bl, "VND") == 0)
+        {
+            bl = new Vnd();
+        }
+
+        // ----------- com TABU -----------
+        if (strcmp(metodo_bl, "TABU") == 0)
+        {
+            int iter_tabu = atoi(argv[6]);
+            int tamanho_lista = atoi(argv[7]);
+            char *bl_tabu = argv[8];
+
+            // ----------- TABU com VNS -----------
+            if (strcmp(bl_tabu, "VNS") == 0)
+            {
+                int iter_bl_aux = atoi(argv[9]);
+                bl_aux = new Vns(iter_bl_aux);
+            }
+            // ----------- TABU com VND -----------
+            else if (strcmp(bl_tabu, "VND") == 0)
+            {
+                bl_aux = new Vnd();
+            }
+            bl = new BuscaTabu(iter_tabu, tamanho_lista, bl_aux);
+        }
+
+        bool is_reativo = alpha < 0;
+
+        solucao_final = Grasp(is_reativo, iter_grasp, alpha, bl).executa(ins);
+
+        delete bl;
+        delete bl_aux;
+    }
+
+    // --------------- VNS ---------------
+    if (strcmp(metaheuristica, "VNS") == 0)
+    {
+        int iter_vns = atoi(argv[3]);
+        char* modo_construcao = argv[4];
+
+        Solucao s(ins);
+        if (strcmp(modo_construcao, "GUL") == 0)
+        {
+            s = Construtor(ins).construcao_gulosa();
+        }
+        if (strcmp(modo_construcao, "AL") == 0)
+        {
+            s = Construtor(ins).construcao_aleatoria();
+        }
+        if (strcmp(modo_construcao, "GUL_AL") == 0)
+        {
+            float alpha_construcao = atof(argv[5]);
+            s = Construtor(ins).construcao_gulosa_aleatoria(alpha_construcao);
+        }
+
+        solucao_final = Vns(iter_vns).executa(s);
+    }
+
+    // --------------- TABU ---------------
+    if (strcmp(metaheuristica, "TABU") == 0)
+    {
+        int iter_tabu = atoi(argv[3]);
+        int tamanho_lista = atoi(argv[4]);
+        char* tipo_bl_tabu = argv[5];
+        char* modo_construcao = argv[6];
+        BuscaLocal* bl_tabu = nullptr;
+
+        Solucao s(ins);
+        if (strcmp(modo_construcao, "GUL") == 0)
+        {
+            s = Construtor(ins).construcao_gulosa();
+        }
+        if (strcmp(modo_construcao, "AL") == 0)
+        {
+            s = Construtor(ins).construcao_aleatoria();
+        }
+        if (strcmp(modo_construcao, "GUL_AL") == 0)
+        {
+            float alpha_construcao = atof(argv[7]);
+            s = Construtor(ins).construcao_gulosa_aleatoria(alpha_construcao);
+        }
+
+        // ----------- TABU com VNS -----------
+        if (strcmp(tipo_bl_tabu, "VNS") == 0)
+        {
+            int iter_bl_aux = atoi(argv[9]);
+            bl_tabu = new Vns(iter_bl_aux);
+        }
+        // ----------- TABU com VND -----------
+        else if (strcmp(tipo_bl_tabu, "VND") == 0)
+        {
+            bl_tabu = new Vnd();
+        }
+
+        solucao_final = BuscaTabu(iter_tabu, tamanho_lista, bl_tabu).executa(s);
+
+        delete bl_tabu;
+    }
+
+    Validador().valida(ins, solucao_final);
+
+    delete ins;
+
+    logger->finaliza_logger();
+
+    return EXIT_SUCCESS;
+}
